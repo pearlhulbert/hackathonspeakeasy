07128bae475611a4346656f9d4075fd2a30c2d56
clamguts <pearl.k.hulbert@gmail.com>
2023-05-16 17:51:13 -0700

    working mic

--- src/MicInput.tsx
+++ src/MicInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useWhisper } from '@chengsokdara/use-whisper'
+import React, { useState, useEffect } from 'react';
+import { useWhisper } from '@chengsokdara/use-whisper';
 import openAIkey from './openAIkey.json';
 
 function MicInput() {
@@ -13,24 +13,47 @@ function MicInput() {
     stopRecording,
   } = useWhisper({
     apiKey: openAIkey['apikey'], // YOUR_OPEN_AI_TOKEN
-  })
+  });
+
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (transcript && transcript.text) {
+      setInputValue(transcript.text);
+    }
+  }, [transcript]);
+
+  function handleResponse() { 
+    clear();
+    // use mimessage and GPT to generate a response
+  }
+
+
+  const clear = () => {
+    setInputValue('');
+  };
 
   return (
-  <div className="MicInput">
-    <button onClick={() => startRecording()}>Listen</button>
-      <input type="text" value={transcript.text} style={{
-        width: '1000px',
-        height: '10vh',
-        fontSize: '16px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: '300px',
-        marginTop: '10px'
-        }} />
-    {/* <button onClick={() => pauseRecording()}>Pause</button> */}
-    <button onClick={() => stopRecording()}>Respond</button>
-  </div>
+    <div className="MicInput">
+      <button onClick={startRecording}>Listen</button>
+      <button onClick={stopRecording}>Stop</button>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        style={{
+          width: '1000px',
+          height: '10vh',
+          fontSize: '16px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginLeft: '300px',
+          marginTop: '10px',
+        }}
+      />
+      <button onClick={handleResponse}>Respond</button>
+    </div>
   );
 }
 
