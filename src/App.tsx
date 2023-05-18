e50f7becf2c1fbdf51a75babce63222e82ed8f04
Jared Hulbert <jaredeh@gmail.com>
2023-05-17 23:05:35 -0700

    partial format of App

--- src/App.tsx
+++ src/App.tsx
@@ -11,76 +11,75 @@ import { green } from '@mui/material/colors';
 import bgImage from './bgimage.png';
 
 const App: React.FC = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [responseValue, setResponseValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [responseValue, setResponseValue] = useState('');
 
-  var input : MicInput = new MicInput(openAIkey['apikey']);
-  var response : ResponseGenerator = new ResponseGenerator();
-  var messageHistory: Array<string> = people['history'];
-  var blob: Blob;
-    
-  const [audioURL, setAudioURL] = useState<string | null>(null); // Initialize the state with a null value
+    var input: MicInput = new MicInput(openAIkey['apikey']);
+    var response: ResponseGenerator = new ResponseGenerator();
+    var messageHistory: Array<string> = people['history'];
+    var blob: Blob;
 
-      useEffect(() => {
+    const [audioURL, setAudioURL] = useState<string | null>(null); // Initialize the state with a null value
+
+    useEffect(() => {
         if (input.transcript && input.transcript.text) {
-          setInputValue(input.transcript.text);
-          var newMessage: string = "**Them**: " + input.transcript.text + "\n";
-          messageHistory.push(newMessage);
-          console.log(messageHistory);
-        }; 
-      }, [input.transcript, messageHistory]);
-    
-      useEffect(() => {
+            setInputValue(input.transcript.text);
+            var newMessage: string = "**Them**: " + input.transcript.text + "\n";
+            messageHistory.push(newMessage);
+            console.log(messageHistory);
+        };
+    }, [input.transcript, messageHistory]);
+
+    useEffect(() => {
         setResponseValue("");
-      }, []);
+    }, []);
 
-      const clear = () => {
+    const clear = () => {
         setInputValue('');
-      };
+    };
 
 
-      function doListen() {
+    function doListen() {
         clear();
         input.startRecording();
-      }
+    }
 
-      function handleResponse() { 
-        const handleSpeak = async(inputstr: string) => {
-          try {
-            blob = await dospeak(inputstr);
-            //console.log(data);
-            console.log("done speak");
-            //const blob = binarytoBlob(data);
-            console.log(blob);
-            const url = URL.createObjectURL(blob);
-            console.log(url);
-            setAudioURL(url); // Update the audioURL state with the returned URL
-          } catch (error) {
-            // Handle any errors that occurred during the dospeak function call
-            console.error('Error occurred while speaking:', error);
-          }
+    function handleResponse() {
+        const handleSpeak = async (inputstr: string) => {
+            try {
+                blob = await dospeak(inputstr);
+                //console.log(data);
+                console.log("done speak");
+                //const blob = binarytoBlob(data);
+                console.log(blob);
+                const url = URL.createObjectURL(blob);
+                console.log(url);
+                setAudioURL(url); // Update the audioURL state with the returned URL
+            } catch (error) {
+                // Handle any errors that occurred during the dospeak function call
+                console.error('Error occurred while speaking:', error);
+            }
         }
-                
-                
+
+
         input.stopRecording();
         //clear();
-          const rawcompletion = response.getCompletion(people['prompt'], messageHistory, inputValue);
-          rawcompletion.then(res => {
+        const rawcompletion = response.getCompletion(people['prompt'], messageHistory, inputValue);
+        rawcompletion.then(res => {
             if (res != null) {
-              setResponseValue(res.content);
-              var newResponse =  "**You**: " + res.content + "\n";
-              messageHistory.push(newResponse);
-              console.log(newResponse);
-              handleSpeak(res.content);
+                setResponseValue(res.content);
+                var newResponse = "**You**: " + res.content + "\n";
+                messageHistory.push(newResponse);
+                console.log(newResponse);
+                handleSpeak(res.content);
             }
-           else {
-              setResponseValue(people['default response']);
-           }
-          });
-      }
+            else {
+                setResponseValue(people['default response']);
+            }
+        });
+    }
 
-    
-      return (
+    return (
         <div style={{
           backgroundImage: `url(${bgImage})`,
           display: 'flex',
@@ -115,9 +114,6 @@ const App: React.FC = () => {
         </div>
         
       );
-      
-      
-      
 };
 
 export default App;
